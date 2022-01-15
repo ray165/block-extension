fa258117392091f4567d8c57ba0bc6d31e96dee9
Raymond <70726900+ray165@users.noreply.github.com>
2022-01-15 12:59:01 -0800

    added skeleton UI

--- client/background.js
+++ client/background.js
@@ -4,3 +4,27 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+
+const testData = [{
+    "moomoo": "cow"
+}, {
+    "chocolate": "milk"
+}, {
+    "apple": "sauce"
+}]
+
+
+
+chrome.storage.local.set({"badDude007": 
+    "blocked because he's annoying"
+}, () => console.log("data sent!"))
+
+chrome.storage.local.get(["badDude007"], (result) => {
+    console.log(result)
+})
+
+// For removal
+chrome.storage.local.clear(() => {
+    console.log('Everything was removed');
+});
