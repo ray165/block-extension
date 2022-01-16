95622f856e3b9d89dc4aede505334da5b8e404a8
Raymond <70726900+ray165@users.noreply.github.com>
2022-01-16 10:29:52 -0800

    added fix for names

--- client/popup.js
+++ client/popup.js
@@ -1,24 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let name = document.getElementById("nameToBlock").value
+    let reason = document.getElementById("descriptionWhy").value
+    
     var submitBlockBtn = document.getElementById('submitBlock')
     submitBlockBtn.addEventListener('click', () => {
         console.log("user is blocked")
-        alert("ok but this works?")
-        let name = document.getElementById("nameToBlock").value
-        let reason = document.getElementById("descriptionWhy").value
-        setData(name, reason)
+        setData(document.getElementById("nameToBlock").value, document.getElementById("descriptionWhy").value)
     })
 
     var searchBtn = document.getElementById('searchBtn')
     searchBtn.addEventListener('click', () => {
         console.log("running search")
+        findData(String(document.getElementById("nameToBlock").value))
     })
 })
 
 
 function setData(name, reason) {
-    chrome.storage.sync.set({
+    chrome.storage.local.set({
         name: reason
     }, () => {
-        console.log("succesfully sent: " ,name ," and ", reason)
+        console.log("succesfully sent: " , name ," and ", reason)
+    })
+}
+
+function findData(name) {
+    chrome.storage.local.get(name, (result) => {
+        console.log(result[name])
+        document.getElementById("descriptionWhy").value = result[name];
     })
 }
